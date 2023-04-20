ee1e7ed318772473d46b4c606e9987239bd78d22
efzr <emerson,zapatarivas@gmail.com>
2023-04-19 23:17:20 -0600

    Paypal side

--- src/Controllers/Checkout/Checkout.php
+++ src/Controllers/Checkout/Checkout.php
@@ -2,35 +2,35 @@
 
 namespace Controllers\Checkout;
 
-use Controllers\PrivateController;
+use Controllers\PublicController;
 
-class Checkout extends PrivateController{
+class Checkout extends PublicController{
     public function run():void
     {
         $viewData = array();
-        $datos = array();
-        if (isset($_SESSION['cart']) && count($_SESSION['cart']) > 0) {
-            $cart = $_SESSION['cart'];
-            $viewData['total'] = 0;
-            foreach ($cart as $key => $value) {
-                $producto = \Dao\Admin\Productos::findById($value['id_producto']);
-                $producto['quantity'] = $value['quantity'];
-                $producto['total'] = $value['quantity'] * $producto['precio'];
-                $datos[] = $producto;
-            }
-        }
-        // print_r($datos);
-
+        $url = getenv('BASE_DIR');
         if ($this->isPostBack()) {
             $PayPalOrder = new \Utilities\Paypal\PayPalOrder(
                 "test".(time() - 10000000),
-                "http://localhost/ProyectoGrupal/index.php?page=checkout_error",
-                "http://localhost/ProyectoGrupal/index.php?page=checkout_accept"
+                "http://localhost/". $url ."/index.php?page=checkout_error",
+                "http://localhost/". $url ."/index.php?page=checkout_accept"
             );
-            foreach($datos as $value){
-                $PayPalOrder->addItem($value["nombre"],$value["descripcion"] , $value["id_producto"], $value["precio"], 0, $value["quantity"], $value["categoria"]);
+            $productos = array();
+            $cart = $_SESSION["cart"];
+            
+            foreach($cart as $key => $value)
+            {
+                $producto = \Dao\Admin\Productos::findById($value["id_producto"]);
+                $producto["quantity"] = $value["quantity"];
+                $productos[] = $producto;
             }
-            print_r($PayPalOrder);
+
+            foreach($productos as $producto)
+            {
+                $PayPalOrder->addItem($producto["nombre"], $producto["nombre"], $producto["id"], $producto["precio"], $producto["precio"] * 0.15, $producto["quantity"], "DIGITAL_GOODS");
+            }
+
+            $PayPalOrder->addItem("Test", "TestItem1", "PRD1", 100, 15, 1, "DIGITAL_GOODS");
             $PayPalOrder->addItem("Test 2", "TestItem2", "PRD2", 50, 7.5, 2, "DIGITAL_GOODS");
             $response = $PayPalOrder->createOrder();
             $_SESSION["orderid"] = $response[1]->result->id;
